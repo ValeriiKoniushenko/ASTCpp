e8d4e4f0db4f01a55def9becaf0556fbe5daf9e8
ValeriiKoniushenko <Valerii.Koniushenko@gmail.com>
2024-08-03 13:34:27 +0300

    Stable version of the Lexer

---
+++ sources/AST/Lexers/BaseLexer.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2024 Valerii Koniushenko
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "BaseLexer.h"
+#include "Core/Assert.h"
+
+namespace Ast
+{
+
+    void LexerLogCollector::AddLog(const LogLine& logLine)
+    {
+        if (Verify(logLine.type != LogType::None, "Was passed LogType::None but expected NOT LogType::None")
+            && Verify(!logLine.message.IsEmpty(), "Was passed an empty message to the log"))
+        {
+            _logs.emplace_back(logLine);
+        }
+    }
+
+} // namespace Ast
---
+++ sources/AST/Lexers/BaseLexer.h
@@ -0,0 +1,117 @@
+// Copyright (c) 2024 Valerii Koniushenko
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include "../CommonTypes.h"
+#include "Utils/CopyableAndMoveableBehaviour.h"
+
+#include <filesystem>
+
+namespace Ast
+{
+
+    class LexerLogCollector : public Utils::CopyableAndMoveable
+    {
+    public:
+        enum class LogType
+        {
+            None,
+            Info,
+            Warning,
+            Error
+        };
+
+        struct LogLine
+        {
+            String message;
+            LogType type = LogType::None;
+        };
+
+        using Container = std::vector<LogLine>;
+
+    public:
+        LexerLogCollector() = default;
+        ~LexerLogCollector() override = default;
+
+        void AddLog(const LogLine& logLine);
+        [[nodiscard]] const Container& GetLogs() const noexcept { return _logs; }
+        [[nodiscard]] bool IsEmpty() const { return _logs.empty(); }
+
+        template<LogType logType>
+        [[nodiscard]] bool HasAny() const
+        {
+            return std::find_if(_logs.cbegin(), _logs.cend(), [](const LogLine& logLine)
+            {
+                return logLine.type == logType;
+            }) != _logs.cend();
+        }
+
+    protected:
+        Container _logs;
+    };
+
+    class BaseLexer : public Utils::CopyableAndMoveable
+    {
+    public:
+        enum class Type
+        {
+            None,
+            Variable,
+            Property,
+            Value,
+            Class,
+            Struct,
+            Namespace,
+            Enum,
+            EnumClass,
+        };
+
+        enum class TypeQualifier
+        {
+            None,
+            Pointer,
+            Reference,
+            Const,
+            Constexpr,
+            Constinit,
+            Consteval,
+            Attribute,
+            Extern,
+            Static
+        };
+
+        [[nodiscard]] virtual bool TryToRecognize(const Ast::String& string) = 0;
+
+        ~BaseLexer() override = default;
+
+    protected:
+        BaseLexer() = default;
+
+    protected:
+        const Char* _tokenStart = nullptr;
+        const Char* _tokenEnd = nullptr;
+        const std::filesystem::path* _filePath = nullptr;
+
+        Type _type = Type::None;
+        TypeQualifier _typeQualifier = TypeQualifier::None;
+    };
+
+}// namespace Ast
