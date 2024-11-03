159701e1c8bfe00ac8eaf27bb78c9ef71850a684
ValeriiKoniushenko <ValeriiKoniushenko@gmail.com>
2024-11-03 23:29:11 +0200

    Refactored CMakeLists.txt

---
+++ cmake/Predefinitions.cmake
@@ -0,0 +1,13 @@
+include_guard()
+
+function(InstallDebugLevel)
+	if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+		add_compile_definitions(AST_DEBUG=1)
+	else()
+		add_compile_definitions(AST_DEBUG=0)
+	endif()
+endfunction()
+
+function(InstallBenchmarkDependenciesTesting)
+	set(BENCHMARK_ENABLE_TESTING OFF PARENT_SCOPE)
+endfunction()
