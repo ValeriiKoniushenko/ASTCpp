3755796546f5f8a79ff88ce05e9542e6d69ed67d
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-02-08 10:48:44 +0200

    Small bug fix

--- sources/AstCpp/Utils/UnitBridge.h
+++ sources/AstCpp/Utils/UnitBridge.h
@@ -214,7 +214,7 @@ namespace Ast::Cpp
                 }
 
                 // trying to find #include or #pragma once
-                if (std::regex_match(i, end, std::regex(R"((^\s*#include)|(^\s*#pragma\s+once))")))
+                if (std::regex_match(i, end, std::regex(R"(^\s*(#include)|(#pragma\s+once))")))
                 {
                     validLine = line + 1;
                 }
