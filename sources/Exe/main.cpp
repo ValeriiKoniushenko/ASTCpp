94b7feae5687d17e0a503bae705d9cf23497f343
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-05-17 22:49:58 +0200

    Added an ability to skip file generation if the main file wasn't modified

--- sources/Exe/main.cpp
+++ sources/Exe/main.cpp
@@ -24,28 +24,6 @@
 #include "AstCpp/Lexers/EnumClassLexer.h"
 #include "AstCpp/ProjectTree.h"
 
-const char* copyrightString = R"(//  MIT License
-//
-//  Copyright (c) 2019-2025 Valerii Koniushenko
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//  SOFTWARE.)";
-
 int main(int argc, char* argv[])
 {
     using namespace Ast;
@@ -74,7 +52,7 @@ int main(int argc, char* argv[])
 
     auto composer = Ast::GeneratorFileComposer::Create();
     composer->addGenerator<Cpp::EnumClassLexer, Cpp::EnumClassGenerator>();
-    composer->setFileHeader(copyrightString + String("\n\n#pragma once\n"));
+    composer->setFileHeader(String("\n\n#pragma once\n"));
 
     project->setGeneratorFileComposer(composer);
     project->generate();
@@ -82,6 +60,7 @@ int main(int argc, char* argv[])
     project->getFSTree()->prettyPrint(
         [](const DiskUnit* unit) -> FSTree::PrettyInfo
         {
+            return { true };
             if (auto* file = dynamic_cast<const FileUnit*>(unit))
             {
                 FSTree::PrettyInfo info;
